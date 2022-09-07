c533b27c3a6da8c2048a29aade7883e176d7df5b
Dominic Rodriquez <nickrsd@gmail.com>
2022-09-06 17:58:59 -0700

    Setup facade and internal session mapping
    setup delegates and handlers, each of which is bound to the stubbed functions that will serve as a facade for session management

--- Plugins/MultiplayerSessionPlugin/Source/MultiplayerSessionPlugin/Private/MultiplayerSessionSubsystem.cpp
+++ Plugins/MultiplayerSessionPlugin/Source/MultiplayerSessionPlugin/Private/MultiplayerSessionSubsystem.cpp
@@ -1,9 +1,61 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MultiplayerSessionSubsystem.h"
+#include "OnlineSubsystem.h"
 
-UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem()
+UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem():
+	// initializing delegates by creating and binding them to the internal delegate functions
+	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &UMultiplayerSessionSubsystem::OnCreateSessionComplete)),
+	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &UMultiplayerSessionSubsystem::OnFindSessionsComplete)),
+	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &UMultiplayerSessionSubsystem::OnJoinSessionComplete)),
+	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &UMultiplayerSessionSubsystem::OnStartSessionComplete)),
+	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &UMultiplayerSessionSubsystem::OnDestroySessionComplete))
 {
-	
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		SessionInterface = Subsystem->GetSessionInterface();
+	}
+}
+
+void UMultiplayerSessionSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+
+void UMultiplayerSessionSubsystem::FindSessions(int32 NumMaxSearchResults)
+{
+}
+
+void UMultiplayerSessionSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
+{
+}
+
+void UMultiplayerSessionSubsystem::StartSession()
+{
+}
+
+void UMultiplayerSessionSubsystem::DestroySession()
+{
+}
+
+
+// internal callbacks for session delegate methods (shouldn't need to modify these)
+void UMultiplayerSessionSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiplayerSessionSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
 }
